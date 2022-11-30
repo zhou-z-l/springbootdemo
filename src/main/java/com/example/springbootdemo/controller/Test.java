ce4a71c63ba47ac16f7bf0a5cbea49da81a6ecdc
zhouzuolin <1065322522@qq.com>
2022-11-30 14:53:26 +0800

    20221130 test

---
+++ src/main/java/com/example/springbootdemo/controller/Test.java
@@ -0,0 +1,18 @@
+package com.example.springbootdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/api/test")
+public class Test {
+
+    @RequestMapping(value="/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello";
+//        System.out.println("hello");
+    }
+
+}
